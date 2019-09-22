f745e41a43c5ee8306fa67ef13a7ce4dc36d3961
sajjad.hussain <saz2nitk@gmail.com>
2019-09-22 16:12:17 +0530

    change port 8080 to 80

--- codes/appRun.py
+++ codes/appRun.py
@@ -36,4 +36,4 @@ def last_words_api():
     return str(last_words)    
 
 if __name__=='__main__':
-    preprocess_app.run(host='0.0.0.0',port=8080,debug=True)
+    preprocess_app.run(host='0.0.0.0',port=80,debug=True)
